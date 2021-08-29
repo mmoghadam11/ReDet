abc82499e08869783cdf93e5b230fcb97e6d5412
Mahdiyar <86521218+mmoghadam11@users.noreply.github.com>
2021-08-29 16:13:42 +0430

    Update __init__.py

--- mmdet/core/bbox/samplers/__init__.py
+++ mmdet/core/bbox/samplers/__init__.py
@@ -8,9 +8,15 @@ from .combined_sampler import CombinedSampler
 from .ohem_sampler import OHEMSampler
 from .sampling_result import SamplingResult
 from .rbbox_random_sampler import RandomRbboxSampler
+###########################################################
+from .rbbox_combined_sampler import CombinedRbboxSampler
+from .rbbox_instance_balanced_pos_sampler import InstanceBalancedPosRbboxSampler
+from .rbbox_iou_balanced_neg_sampler import IoUBalancedNegRbboxSampler
 
 __all__ = [
     'BaseSampler', 'RbboxBaseSampler', 'PseudoSampler', 'RandomSampler',
     'InstanceBalancedPosSampler', 'IoUBalancedNegSampler', 'CombinedSampler',
-    'OHEMSampler', 'SamplingResult', 'RandomRbboxSampler'
+    'OHEMSampler', 'SamplingResult', 'RandomRbboxSampler',
+    ########################################################
+    'CombinedRbboxSampler','InstanceBalancedPosRbboxSampler','IoUBalancedNegRbboxSampler'
 ]
