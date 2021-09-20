d37766e7d21dfd5c04bea97bc80a5c8dec49f078
Mahdiyar <86521218+mmoghadam11@users.noreply.github.com>
2021-09-21 01:37:28 +0430

    Update __init__.py

--- mmdet/models/losses/__init__.py
+++ mmdet/models/losses/__init__.py
@@ -5,12 +5,12 @@ from .ghm_loss import GHMC, GHMR
 from .balanced_l1_loss import BalancedL1Loss
 from .iou_loss import (BoundedIoULoss, CIoULoss, DIoULoss, GIoULoss, IoULoss,
                        bounded_iou_loss, iou_loss)
-# from .obb.poly_iou_loss import PolyIoULoss, PolyGIoULoss
+from .obb.poly_iou_loss import PolyIoULoss, PolyGIoULoss
 __all__ = [
     'CrossEntropyLoss', 'FocalLoss', 'SmoothL1Loss', 'BalancedL1Loss',
     'IoULoss', 'GHMC', 'GHMR'
     ####################################################
     ,'BoundedIoULoss', 'CIoULoss', 'DIoULoss', 'GIoULoss', 'IoULoss',
                       'bounded_iou_loss' , 'iou_loss',
-#   'PolyGIoULoss','PolyIoULoss'
+  'PolyGIoULoss','PolyIoULoss'
 ]
