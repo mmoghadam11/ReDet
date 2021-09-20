2f9778992a5340de63e1a372c9c10db9ce792dec
Mahdiyar <86521218+mmoghadam11@users.noreply.github.com>
2021-09-21 04:17:37 +0430

    Update setup.py

--- mmdet/ops/convex/setup.py
+++ mmdet/ops/convex/setup.py
@@ -8,8 +8,9 @@ setup(
         CUDAExtension('convex_ext', [
             'src/convex_cpu.cpp',
             'src/convex_ext.cpp',
-        ],
-        sources_cuda=['src/convex_cuda.cu']
+          'src/convex_cuda.cu'
+        ]
+#         sources_cuda=['src/convex_cuda.cu']
         ),
     ],
     cmdclass={'build_ext': BuildExtension})
