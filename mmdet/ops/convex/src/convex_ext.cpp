917d370c827b65f0dc3618899290fd9288f30f64
Mahdiyar <86521218+mmoghadam11@users.noreply.github.com>
2021-09-28 08:28:56 +0330

    Update convex_ext.cpp

--- mmdet/ops/convex/src/convex_ext.cpp
+++ mmdet/ops/convex/src/convex_ext.cpp
@@ -1,25 +1,25 @@
 #include <ATen/ATen.h>
 #include <torch/extension.h>
 
-#ifdef WITH_CUDA
+// #ifdef WITH_CUDA
 at::Tensor convex_sort_cuda(
     const at::Tensor& pts, const at::Tensor& masks, const bool circular);
-#endif
+// #endif
 
-at::Tensor convex_sort_cpu(
-    const at::Tensor& pts, const at::Tensor& masks, const bool circular);
+// at::Tensor convex_sort_cpu(
+//     const at::Tensor& pts, const at::Tensor& masks, const bool circular);
 
 
 at::Tensor convex_sort(
     const at::Tensor& pts, const at::Tensor& masks, const bool circular) {
-  if (pts.device().is_cuda()) {
-#ifdef WITH_CUDA
+//   if (pts.device().is_cuda()) {
+// #ifdef WITH_CUDA
     return convex_sort_cuda(pts, masks, circular);
-#else
-    AT_ERROR("sort_vert is not compiled with GPU support");
-#endif
-  }
-  return convex_sort_cpu(pts, masks, circular);
+// #else
+//     AT_ERROR("sort_vert is not compiled with GPU support");
+// #endif
+//   }
+//   return convex_sort_cpu(pts, masks, circular);
 }
 
 
